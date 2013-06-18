f852f2815db5a5552ea957d5b373f983e89c0d33
Casey Moncur <caseymonc@gmail.com>
2013-06-17 20:40:58 -0600

    Initial Commit

---
+++ TwentyMinuteGenealogist/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TwentyMinuteGenealogist
+//
+//  Created by Casey Moncur on 6/17/13.
+//  Copyright (c) 2013 KinPoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TMGMainAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TMGMainAppDelegate class]));
+    }
+}
